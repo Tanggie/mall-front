21b5e8e3b39deb2e126ba375f2e0e8b9de8b650f
Tanggie <46233913+Tanggie@users.noreply.github.com>
2022-06-21 17:08:49 +0800

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,16 +1,16 @@
 let baseURL;
 switch (process.env.NODE_ENV) {
   case 'development':
-    baseURL = 'http://localhost:8887/csu_super_mall/api';
+    baseURL = 'http://localhost:8887/csu_super_mall/';
     break;
   case 'test':
     baseURL = 'http://localhost:8887/csu_super_mall/';
     break;
   case 'prev':
-    baseURL = 'http://prev-mall-pre.springboot.cn/api';
+    baseURL = 'http://localhost:8887/csu_super_mall/';
     break;
   case 'prod':
-    baseURL = 'http://dev-mall-pre.springboot.cn/api/';
+    baseURL = 'http://localhost:8887/csu_super_mall/';
     break;
   default:
     baseURL = 'http://localhost:8887/csu_super_mall/';
